4b052a422f4fd305114afb9e808032f6b0148f7d
iceefast <samuel.cristian17062004@gmail.com>
2023-09-12 22:55:17 -0300

    modularizing the focustimer elements

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import "./toggle-mode.js";
 import * as FocusTimer from "./focusTimer/index.js";
+import * as events from "./focusTimer/events.js";
 
 FocusTimer.start(0, 6);
 console.log(FocusTimer);
+console.log(events);
